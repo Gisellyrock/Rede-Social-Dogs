5d1f597dabb9d794344e62b2810315050ef0bd34
GisellyRock <gisellydsr@gmail.com>
2023-04-11 12:06:05 -0300

    adding fetch

--- src/Components/Login/LoginForm.js
+++ src/Components/Login/LoginForm.js
@@ -3,6 +3,7 @@ import { Link, useFetcher } from 'react-router-dom';
 import Input from '../Forms/Input';
 import Button from '../Forms/Button';
 import useForm from '../../Hooks/useForm';
+import { TOKEN_POST } from '../../api';
 
 const LoginForm = () => {
   const username = useForm();
@@ -14,22 +15,16 @@ const LoginForm = () => {
     event.preventDefault();
 
     if (username.validate() && password.validate()) {
-      fetch('https://dogsapi.origamid.dev/json/jwt-auth/v1/token', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(),
-      })
-        .then((response) => {
-          console.log(response);
-          return response.json();
-        })
-        .then((json) => {
-          console.log(json);
-        });
-    }
+      const { url, options } = TOKEN_POST({
+        username: username.value,
+        password: password.value,
+      });
+
+  const response = await fetch(url, options)
+  const json = await response.json();
+  console.log(json);
   }
+}
 
   return (
     <section>
