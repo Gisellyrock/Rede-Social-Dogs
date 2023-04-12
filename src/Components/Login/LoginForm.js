02bf0b5d4377fa1ac44bd63df014114b3893fa4d
GisellyRock <gisellydsr@gmail.com>
2023-04-12 20:00:52 -0300

    adding API

--- src/Components/Login/LoginForm.js
+++ src/Components/Login/LoginForm.js
@@ -1,17 +1,29 @@
 import React from 'react';
-import { Link, useFetcher } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import Input from '../Forms/Input';
 import Button from '../Forms/Button';
 import useForm from '../../Hooks/useForm';
-import { TOKEN_POST } from '../../api';
+import { TOKEN_POST, USER_GET } from '../../api';
 
 const LoginForm = () => {
   const username = useForm();
   const password = useForm();
 
-  console.log(password.value);
+  React.useEffect(() => {
+    const token = window.localStorage.getItem('token');
+    if (token) {
+      getUser(token);
+    }
+  }, []);
 
-  function handleSubmit(event) {
+  async function getUser(token) {
+    const { url, options } = USER_GET(token);
+    const response = await fetch(url, options);
+    const json = await response.json();
+    console.log(json);
+  }
+
+  async function handleSubmit(event) {
     event.preventDefault();
 
     if (username.validate() && password.validate()) {
@@ -20,11 +32,12 @@ const LoginForm = () => {
         password: password.value,
       });
 
-  const response = await fetch(url, options)
-  const json = await response.json();
-  console.log(json);
+      const response = await fetch(url, options);
+      const json = await response.json();
+      window.localStorage.setItem('token', json.token);
+      getUser(json.token);
+    }
   }
-}
 
   return (
     <section>
