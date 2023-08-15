6a318c9a9240f6bf3fbae7b7a212f70051658bb2
GisellyRock <gisellydsr@gmail.com>
2023-08-15 10:16:19 -0300

    feat: loading

--- src/Components/Feed/Feed.js
+++ src/Components/Feed/Feed.js
@@ -2,15 +2,29 @@ import React from 'react';
 import FeedPhotos from './FeedPhotos';
 import FeedModal from './FeedModal';
 
-const Feed = () => {
+const Feed = ({ user }) => {
   const [modalPhoto, setModalPhoto] = React.useState(null);
+  // const [pages, setPages] =
+  React.useEffect(() => {
+    function infiniteScroll(event) {
+      console.log(event);
+    }
+
+    window.addEventListener('wheel', infiniteScroll);
+    window.addEventListener('scroll', infiniteScroll);
+    return () => {
+      window.addEventListener('wheel', infiniteScroll);
+      window.addEventListener('scroll', infiniteScroll);
+    };
+  }, []);
 
   return (
     <div>
       {modalPhoto && (
         <FeedModal photo={modalPhoto} setModalPhoto={setModalPhoto} />
       )}
-      <FeedPhotos setModalPhoto={setModalPhoto} />
+      <FeedPhotos user={user} page="1" setModalPhoto={setModalPhoto} />
+      <FeedPhotos user={user} page="2" setModalPhoto={setModalPhoto} />
     </div>
   );
 };
